ac983306e0c14d3740a38d294fa56cb04c96ad7c
Alex Pry <alhaffthemage@gmail.com>
2022-09-11 17:01:41 +0300

    Added comments to interface

--- CooridnateGrid/CooridnateGrid/DrawnObjects/IDrawnObject.cs
+++ CooridnateGrid/CooridnateGrid/DrawnObjects/IDrawnObject.cs
@@ -10,10 +10,24 @@ using System.Windows.Media.Imaging;
 
 namespace CooridnateGrid.DrawnObjects
 {
+    /// <summary>
+    /// Інтерфейс, що мають успадковувати об'єкти, які мають бути намальовані
+    /// </summary>
     public interface IDrawnObject : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Повертає перелік точок контуру рисунку. Так як об'єкт може мати не суцільний контур 
+        /// повертає його у вигляді переліку точок які можна з'єднати нерозривною лінією.
+        /// </summary>
+        /// <returns>type - IEnumerable<IEnumerable<Vector2>></returns>
         public IEnumerable<IEnumerable<Vector2>> GetContourPoints();
+        /// <summary>
+        /// Колір лінії контуру об'єкта
+        /// </summary>
         public Color MyColor { get; set; }
+        /// <summary>
+        /// Функція, що буду застосована, до всіх точок об'єкту
+        /// </summary>
         public Func<Vector2, Vector2> TransformFunctions { get; set; }
     }
 }
