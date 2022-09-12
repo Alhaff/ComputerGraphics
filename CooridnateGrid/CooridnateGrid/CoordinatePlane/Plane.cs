50c47f2aa36cebf93fd45cbf9cf26d6673de611f
Alex Pry <alhaffthemage@gmail.com>
2022-09-12 19:50:48 +0300

    Change name from IDrawObject to DrawnObject

--- CooridnateGrid/CooridnateGrid/CoordinatePlane/Plane.cs
+++ CooridnateGrid/CooridnateGrid/CoordinatePlane/Plane.cs
@@ -11,26 +11,26 @@ namespace CooridnateGrid.CoordinatePlane
 {
     public abstract class Plane
     {
-        public List<IDrawnObject> Objects { get; }
+        public List<DrawnObject> Objects { get; }
 
         public  WriteableBitmap WrBitmap { get; set; }
         public readonly int StepInPixels;
         public Plane(int bitmapWidth, int bitmapHeight, int stepInPixels)
         {
-            Objects = new List<IDrawnObject>();
+            Objects = new List<DrawnObject>();
             WrBitmap = BitmapFactory.New(bitmapWidth, bitmapHeight);
             StepInPixels = stepInPixels;
         }
-        public virtual void AddObject(IDrawnObject obj)
+        public virtual void AddObject(DrawnObject obj)
         {
             Objects.Add(obj);
         }
 
-        public virtual void RemoveObject(IDrawnObject obj)
+        public virtual void RemoveObject(DrawnObject obj)
         {
             Objects.Remove(obj);
         }
-        private void DrawObj(IDrawnObject obj)
+        private void DrawObj(DrawnObject obj)
         {
             var lineBuilder = new LinkedList<Vector2>();
             foreach (var contour in obj.GetContourPoints())
