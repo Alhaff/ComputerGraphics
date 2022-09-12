9f387c2f40e21428ce33efb7857563dd9be6c988
Alex Pry <alhaffthemage@gmail.com>
2022-09-12 19:06:13 +0300

    Added extensions to Vector2 class, which added Rotate, and Angle functions

---
+++ CooridnateGrid/CooridnateGrid/DrawnObjects/Vector2Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CooridnateGrid.DrawnObjects
+{
+    public static class Vector2Extensions
+    {
+        public static Vector2 Rotate(this Vector2 start, double angle)
+        {
+            float newX = (float)(start.X * Math.Cos(angle) - start.Y * Math.Sin(angle));
+            float newY = (float)(start.X * Math.Sin(angle) + start.Y * Math.Cos(angle));
+            return new Vector2(newX,newY);
+        }
+
+        public static double Angle(this Vector2 first) => Math.Atan2(first.Y, first.X);
+    }
+}
