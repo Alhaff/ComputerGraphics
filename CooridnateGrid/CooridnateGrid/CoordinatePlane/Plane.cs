27b01b40e22b5baf54e5ee9b6658ca5a272fc35e
Alex Pry <alhaffthemage@gmail.com>
2022-09-14 12:31:53 +0300

    Make plane class non abstract, add realization for previous abstract method, add transform property, rebase to Vector3

--- CooridnateGrid/CooridnateGrid/CoordinatePlane/Plane.cs
+++ CooridnateGrid/CooridnateGrid/CoordinatePlane/Plane.cs
@@ -1,8 +1,10 @@
 ﻿using CooridnateGrid.DrawnObjects;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -10,33 +12,33 @@ using System.Windows.Media.Imaging;
 
 namespace CooridnateGrid.CoordinatePlane
 {
-    public abstract class Plane
+    public class MyPlane
     {
+        public Func<Vector3, Vector3> Transform { get; set; } = v => v;
         public List<DrawnObject> Objects { get; }
-
         public  WriteableBitmap WrBitmap { get; set; }
         public readonly int StepInPixels;
-        public Plane(int bitmapWidth, int bitmapHeight, int stepInPixels)
+        public MyPlane(int bitmapWidth, int bitmapHeight, int stepInPixels)
         {
             Objects = new List<DrawnObject>();
             WrBitmap = BitmapFactory.New(bitmapWidth, bitmapHeight);
             StepInPixels = stepInPixels;
         }
-        public virtual void AddObject(DrawnObject obj)
+        public void AddObject(DrawnObject obj)
         {
             Objects.Add(obj);
         }
 
-        public virtual void RemoveObject(DrawnObject obj)
+        public void RemoveObject(DrawnObject obj)
         {
             Objects.Remove(obj);
         }
         internal void DrawObj(DrawnObject obj)
         {
-            var lineBuilder = new LinkedList<Vector2>();
+            var lineBuilder = new LinkedList<Vector3>();
             foreach (var contour in obj.GetContourPoints())
             {
-                foreach (var points in contour.Select(point => ToBitmapCoord(obj.TransformFunctions(point)))
+                foreach (var points in contour.Select(point => ToBitmapCoord(obj.TransformMe(point)))
                                               .LineCreator())
                 {
                         WrBitmap.DrawLine((int)points.Item1.X, (int)points.Item1.Y,
@@ -44,7 +46,7 @@ namespace CooridnateGrid.CoordinatePlane
                 }
             }
         }
-        public virtual void Draw()
+        public void Draw()
         {
                 WrBitmap.Clear();
                 foreach (var obj in Objects)
@@ -58,21 +60,27 @@ namespace CooridnateGrid.CoordinatePlane
                     }
                 }
         }
+       
+        public virtual Vector3 ToBitmapCoord(Vector3 planeCoord)
+        {
+            var transformed = Transform(planeCoord);
+            return new Vector3((float)(transformed.X * StepInPixels + WrBitmap.Width / 2),
+                               (float)(-(transformed.Y * StepInPixels) + WrBitmap.Height / 2),
+                               1);
+        }
+
         internal static System.Drawing.Imaging.PixelFormat ConvertPixelFormat(System.Windows.Media.PixelFormat sourceFormat)
         {
-            if( PixelFormats.Bgr24 == sourceFormat)
-                    return System.Drawing.Imaging.PixelFormat.Format24bppRgb;    
+            if (PixelFormats.Bgr24 == sourceFormat)
+                return System.Drawing.Imaging.PixelFormat.Format24bppRgb;
             if (PixelFormats.Bgra32 == sourceFormat)
-                    return System.Drawing.Imaging.PixelFormat.Format32bppArgb;
+                return System.Drawing.Imaging.PixelFormat.Format32bppArgb;
             if (PixelFormats.Bgr32 == sourceFormat)
-                    return System.Drawing.Imaging.PixelFormat.Format32bppRgb;
+                return System.Drawing.Imaging.PixelFormat.Format32bppRgb;
             if (PixelFormats.Pbgra32 == sourceFormat)
                 return System.Drawing.Imaging.PixelFormat.Format32bppArgb;
             return new System.Drawing.Imaging.PixelFormat();
         }
-        public virtual Vector2 ToBitmapCoord(Vector2 planeCoord)
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
