f8ef94ca080e2b1c191df6f071829765f15db989
Alex Pry <alhaffthemage@gmail.com>
2022-09-14 12:35:07 +0300

    Rebase to Vector3, change drawing realization( because plane class was changed)

--- CooridnateGrid/CooridnateGrid/DrawnObjects/CoordinateAxis.cs
+++ CooridnateGrid/CooridnateGrid/DrawnObjects/CoordinateAxis.cs
@@ -58,23 +58,23 @@ namespace CooridnateGrid.DrawnObjects
             MyColor = color;
         }
 
-        private Vector2 Point(int x, int y) => new Vector2(x,  y);
-        private Vector2 Point(double x, double y) => new Vector2((float)x, (float)y);
-        private IEnumerable<Vector2> GetAbscissaLine(int y)
+        private Vector3 Point(int x, int y) => TransformMe(new Vector3(x,  y, 1));
+        private Vector3 Point(double x, double y) => TransformMe(new Vector3((float)x, (float)y,1));
+        private IEnumerable<Vector3> GetAbscissaLine(int y)
         {
             for (int x = -CellAmountOnAbscissaAxe / 2; x <= CellAmountOnAbscissaAxe / 2; x++)
             {
                 yield return Point(x, y);
             }
         }
-        private IEnumerable<Vector2> GetOrdinateLine(int x)
+        private IEnumerable<Vector3> GetOrdinateLine(int x)
         {
             for (int y = -CellAmountOnOrdinateAxe / 2; y <= CellAmountOnOrdinateAxe / 2; y++)
             {
                 yield return Point(x, y);
             }
         }
-        public override IEnumerable<IEnumerable<Vector2>> GetContourPoints()
+        public override IEnumerable<IEnumerable<Vector3>> GetContourPoints()
         {
             for(var x = -CellAmountOnAbscissaAxe /2; x <= CellAmountOnAbscissaAxe/2; x++)
             {
@@ -88,18 +88,18 @@ namespace CooridnateGrid.DrawnObjects
                     yield return GetAbscissaLine(y);
             }
         }
-        private void DrawOrdinateAndAbscissaAxes(CoordinatePlane.Plane pl)
+        private void DrawOrdinateAndAbscissaAxes(CoordinatePlane.MyPlane pl)
         {
             pl.DrawObj(this);
 
-            foreach (var points in GetAbscissaLine(0).Select(point => pl.ToBitmapCoord(TransformFunctions(point)))
+            foreach (var points in GetAbscissaLine(0).Select(point => pl.ToBitmapCoord(point))
                                               .LineCreator())
             {
                 pl.WrBitmap.DrawLine((int)points.Item1.X, (int)points.Item1.Y,
                            (int)points.Item2.X, (int)points.Item2.Y, System.Windows.Media.Color.FromRgb(0, 0, 255));
             }
 
-            foreach (var points in GetOrdinateLine(0).Select(point => pl.ToBitmapCoord(TransformFunctions(point)))
+            foreach (var points in GetOrdinateLine(0).Select(point => pl.ToBitmapCoord(point))
                                               .LineCreator())
             {
                 pl.WrBitmap.DrawLine((int)points.Item1.X, (int)points.Item1.Y,
@@ -107,56 +107,57 @@ namespace CooridnateGrid.DrawnObjects
             }
             DrawElements(pl);
         }
-        private void DrawElements(CoordinatePlane.Plane pl)
+        private void DrawElements(CoordinatePlane.MyPlane pl)
         {
-            var abscissaEnd = pl.ToBitmapCoord(TransformFunctions(Point(CellAmountOnAbscissaAxe/2, 0)));
-            var ordinateEnd = pl.ToBitmapCoord(TransformFunctions(Point(0, CellAmountOnOrdinateAxe/2)));
+            var abscissaEnd = pl.ToBitmapCoord(Point(CellAmountOnAbscissaAxe/2, 0));
+            var ordinateEnd = pl.ToBitmapCoord(Point(0, CellAmountOnOrdinateAxe/2));
             var k = 0.6;
             var k1 = 0.4;
-            var abscissaUp = pl.ToBitmapCoord(TransformFunctions(Point(CellAmountOnAbscissaAxe / 2 -k, k1)));
-            var abscissaDown = pl.ToBitmapCoord(TransformFunctions(Point(CellAmountOnAbscissaAxe / 2 -k, -k1)));
-            var ordinateRight = pl.ToBitmapCoord(TransformFunctions(Point(k1, CellAmountOnOrdinateAxe / 2 - k)));
-            var ordinateLeft = pl.ToBitmapCoord(TransformFunctions(Point(-k1, CellAmountOnOrdinateAxe / 2 - k)));
-            var temp1 = pl.ToBitmapCoord(TransformFunctions(Point(1,0.3)));
-            var temp2 = pl.ToBitmapCoord(TransformFunctions(Point(1, -0.3)));
+            var abscissaUp = pl.ToBitmapCoord(Point(CellAmountOnAbscissaAxe / 2 -k, k1));
+            var abscissaDown = pl.ToBitmapCoord(Point(CellAmountOnAbscissaAxe / 2 -k, -k1));
+            var ordinateRight = pl.ToBitmapCoord(Point(k1, CellAmountOnOrdinateAxe / 2 - k));
+            var ordinateLeft = pl.ToBitmapCoord(Point(-k1, CellAmountOnOrdinateAxe / 2 - k));
+            var temp1 = pl.ToBitmapCoord(Point(1,0.3));
+            var temp2 = pl.ToBitmapCoord(Point(1, -0.3));
 
             pl.WrBitmap.DrawLine((int)abscissaEnd.X, (int)abscissaEnd.Y, (int)abscissaUp.X,
                 (int)abscissaUp.Y, System.Windows.Media.Color.FromRgb(0, 0, 255));
-            pl.WrBitmap.DrawLine((int)abscissaEnd.X, (int)abscissaEnd.Y, (int)abscissaUp.X,
+            pl.WrBitmap.DrawLine((int)abscissaEnd.X, (int)abscissaEnd.Y, (int)abscissaDown.X,
               (int)abscissaDown.Y, System.Windows.Media.Color.FromRgb(0, 0, 255));
            
             pl.WrBitmap.DrawLine((int)ordinateEnd.X, (int)ordinateEnd.Y, (int)ordinateRight.X,
                (int)ordinateRight.Y, System.Windows.Media.Color.FromRgb(0, 0, 255));
             pl.WrBitmap.DrawLine((int)ordinateEnd.X, (int)ordinateEnd.Y, (int)ordinateLeft.X,
-              (int)ordinateRight.Y, System.Windows.Media.Color.FromRgb(0, 0, 255));
+              (int)ordinateLeft.Y, System.Windows.Media.Color.FromRgb(0, 0, 255));
 
             pl.WrBitmap.DrawLine((int)temp1.X, (int)temp1.Y, (int)temp2.X,
                (int)temp2.Y, System.Windows.Media.Color.FromRgb(255, 0, 0));
         }
 
-        private void DrawText(CoordinatePlane.Plane pl)
+        private void DrawText(CoordinatePlane.MyPlane pl)
         {
             var wr = pl.WrBitmap;
             var w = wr.PixelWidth;
             var h = wr.PixelHeight;
             var stride = wr.BackBufferStride;
             var pixelPtr = wr.BackBuffer;
-            var bm2 = new Bitmap(w, h, stride, CoordinatePlane.Plane.ConvertPixelFormat(wr.Format), pixelPtr);
-
+            var bm2 = new Bitmap(w, h, stride, CoordinatePlane.MyPlane.ConvertPixelFormat(wr.Format), pixelPtr);
+            var xPoint = pl.ToBitmapCoord(Point(CellAmountOnAbscissaAxe / 2 - 1.5, 0 - 0.5));
+            var yPoint = pl.ToBitmapCoord(Point(0 + 0.5, CellAmountOnOrdinateAxe / 2 - 0.5));
+            var zeroPoint = pl.ToBitmapCoord(Point(0 + 0.1, 0 + 0.1));
+            var onePoint = pl.ToBitmapCoord(Point(1 + 0.1, 0 + 0.1));
             wr.Lock();
-            // you might wanna use this in combination with Lock / Unlock, AddDirtyRect, Freeze
-            // before you write to the shared Ptr
             using (var g = Graphics.FromImage(bm2))
             {
-                g.DrawString("X", new Font("Times New Roman", 12), System.Drawing.Brushes.Blue, (float)(wr.Width - 20f), (float)(wr.Height / 2 + 5f));
-                g.DrawString("Y", new Font("Times New Roman", 12), System.Drawing.Brushes.Blue, (float)(wr.Width / 2 + 10f), 5f);
-                g.DrawString("0", new Font("Times New Roman", 10), System.Drawing.Brushes.Blue, (float)(wr.Width / 2 + 2f), (float)(wr.Height / 2 + 2f));
-                g.DrawString("1", new Font("Times New Roman", 10), System.Drawing.Brushes.Blue, (float)(wr.Width / 2 + 22f), (float)(wr.Height / 2 + 2f));
+                g.DrawString("X", new Font("Times New Roman", 12), System.Drawing.Brushes.Blue, xPoint.X, xPoint.Y);
+                g.DrawString("Y", new Font("Times New Roman", 12), System.Drawing.Brushes.Blue, yPoint.X, yPoint.Y);
+                g.DrawString("0", new Font("Times New Roman", 10), System.Drawing.Brushes.Blue, zeroPoint.X, zeroPoint.Y);
+                g.DrawString("1", new Font("Times New Roman", 10), System.Drawing.Brushes.Blue, onePoint.X, onePoint.Y);
             }
             wr.AddDirtyRect(new Int32Rect(0, 0, 200, 100));
             wr.Unlock();
         }
-        public void Draw(CoordinatePlane.Plane pl)
+        public void Draw(CoordinatePlane.MyPlane pl)
         {
             DrawOrdinateAndAbscissaAxes(pl);
             DrawText(pl);
