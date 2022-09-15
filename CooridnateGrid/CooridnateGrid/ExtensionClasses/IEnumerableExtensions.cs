842d1d36df84d12f7ec16b4d3e5fe47c9fefc04c
Alex Pry <alhaffthemage@gmail.com>
2022-09-15 21:36:22 +0300

    Renamed namespace

--- CooridnateGrid/CooridnateGrid/ExtensionClasses/IEnumerableExtensions.cs
+++ CooridnateGrid/CooridnateGrid/ExtensionClasses/IEnumerableExtensions.cs
@@ -5,7 +5,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CooridnateGrid.DrawnObjects
+namespace CooridnateGrid.ExtensionsClasses
 {
 	public static class IEnumerableExtensions
 	{
--- CooridnateGrid/CooridnateGrid/ExtensionClasses/VectorExtensions.cs
+++ CooridnateGrid/CooridnateGrid/ExtensionClasses/VectorExtensions.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CooridnateGrid.DrawnObjects
+namespace CooridnateGrid.ExtensionsClasses
 {
-    public static class VectorExtensions
+    public static class VectorExtensions 
     {
         public static Vector3 Rotate(this Vector3 start, double angle)
         {
             float newX = (float)(start.X * Math.Cos(angle) - start.Y * Math.Sin(angle));
             float newY = (float)(start.X * Math.Sin(angle) + start.Y * Math.Cos(angle));
-            return new Vector3(newX,newY,1);
+            return new Vector3(newX, newY, 1);
         }
-
+       
         public static float LenFor2Dimension(this Vector3 v)
         {
             var copy = new Vector2(v.X, v.Y);
