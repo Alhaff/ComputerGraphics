fa2b57f92915bda6c60002e323a1f50ace60a99f
Alex Pry <alhaffthemage@gmail.com>
2022-09-16 17:52:41 +0300

    Added new transformation for move object

---
+++ CooridnateGrid/CooridnateGrid/Transformation/MoveTransformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CooridnateGrid.Transformation
+{
+    public class MoveTransformation : Transformation
+    {
+        private float _dX = 0;
+        private float _dY = 0;
+
+        public float dX
+        {
+            get { return _dX; }
+            set { 
+                _dX = value;
+                OnPropertyChanged("dX");
+                }
+        }
+
+        public float dY
+        {
+            get { return _dY; }
+            set { 
+                _dY = value;
+                OnPropertyChanged("dY");
+            }
+        }
+        private Func<Vector3, Vector3> F { get => v => v + new Vector3(dX, dY, 0); } 
+        public override Func<Vector3, Vector3> Transform => F;       
+    }
+}
