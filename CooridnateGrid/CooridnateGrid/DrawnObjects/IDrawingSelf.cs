d41e82c2e7cc26592d8f4afb548414ed8b60801b
Alex Pry <alhaffthemage@gmail.com>
2022-09-20 21:32:11 +0300

    Changed IDrawingSelf for new Drawing model

--- CooridnateGrid/CooridnateGrid/DrawnObjects/IDrawingSelf.cs
+++ CooridnateGrid/CooridnateGrid/DrawnObjects/IDrawingSelf.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,6 @@ namespace CooridnateGrid.DrawnObjects
 {
     public interface IDrawingSelf
     {
-        public void Draw(CoordinatePlane.MyPlane pl);
+        public void Draw(CoordinatePlane.MyPlane pl, Graphics g);
     }
 }
