0c8efae0cda2d2e014b3d20a1d686a5faad53625
Alex Pry <alhaffthemage@gmail.com>
2022-09-26 14:05:35 +0300

    Added to DrawLine functions checking for float value restrictions. This  implemented with new function Is Normal Value. Added 3 new property for control camera on plane, this is Dx, Dy and scale coeficient. Implemented functional, which give ability not apply transformation from plane side to it's object.

--- CooridnateGrid/CooridnateGrid/CoordinatePlane/Plane.cs
+++ CooridnateGrid/CooridnateGrid/CoordinatePlane/Plane.cs
@@ -18,6 +18,7 @@ namespace CooridnateGrid.CoordinatePlane
     {
         #region Variables
         private int _stepInPixels = 20;
+        private double _skaleCoef = 1;
         #endregion
 
         #region Propreties
@@ -39,6 +40,18 @@ namespace CooridnateGrid.CoordinatePlane
                     }
                 }
             
+        }
+
+        public float Dx { get; set; } = 0;
+        public float Dy { get; set; } = 0;
+
+        public double ScaleCoef
+        {
+            get => _skaleCoef;
+            set
+                {
+                    if(value > 0) _skaleCoef = value;
+                }
         }
         #endregion
 
@@ -73,21 +86,33 @@ namespace CooridnateGrid.CoordinatePlane
             var lineBuilder = new LinkedList<Vector3>();
             foreach (var contour in obj.GetContourPoints())
             {
-                foreach (var points in contour.Select(point => ToBitmapCoord(point))
+                Func<Vector3,Vector3> ToBitmap = obj.IsPlaneTransfromMe ? ToBitmapCoord : ToBitmapCoordWithoutTransform;
+                foreach (var points in contour.Select(point => ToBitmap(point))
                                               .LineCreator())
                 {
-                    DrawLine(g, points, obj.MyColor);
+                     DrawLine(g, points, obj.MyColor);
                 }
+                
             }
+        }
+        public static bool IsNormalValue(Vector3 point)
+        {
+            return float.IsFinite(point.X) && float.IsFinite(point.Y);
+                
         }
         internal void DrawLine(Graphics g, Tuple<Vector3,Vector3> points, System.Windows.Media.Color Color)
         {
             System.Drawing.Color color =
                         System.Drawing.Color.FromArgb(Color.A, Color.R, Color.G, Color.B);
             System.Drawing.Pen pen = new System.Drawing.Pen(new SolidBrush(color));
-            Point start = new Point((int)points.Item1.X, (int)points.Item1.Y);
-            Point end = new Point((int)points.Item2.X, (int)points.Item2.Y);
-            g.DrawLine(pen, start, end);
+            var vectStart = points.Item1;
+            var vectEnd = points.Item2;
+            if (IsNormalValue(vectStart) && IsNormalValue(vectEnd))
+            {
+                Point start = new Point((int)vectStart.X, (int)vectStart.Y);
+                Point end = new Point((int)vectEnd.X, (int)vectEnd.Y);
+                g.DrawLine(pen, start, end);
+            }
         }
         public void AddObject(DrawnObject obj)
         {
@@ -122,9 +147,16 @@ namespace CooridnateGrid.CoordinatePlane
         public Vector3 ToBitmapCoord(Vector3 planeCoord)
         {
             var transformed = Transform(planeCoord);
-            return new Vector3((float)(transformed.X * StepInPixels + BitmapWidth / 2),
-                               (float)(-(transformed.Y * StepInPixels) + BitmapHeight / 2),
-                               1);
+            var x = (transformed.X * StepInPixels)/ ScaleCoef + BitmapWidth / 2 + Dx;
+            var y = (-(transformed.Y * StepInPixels)/ ScaleCoef + BitmapHeight / 2) + Dy;
+            return new Vector3((float)x, (float)y, 1);
+        }
+
+        public Vector3 ToBitmapCoordWithoutTransform(Vector3 planeCoord)
+        {
+            var x = (planeCoord.X * StepInPixels) / ScaleCoef + BitmapWidth / 2 + Dx;
+            var y = -(planeCoord.Y * StepInPixels) / ScaleCoef + BitmapHeight / 2 + Dy;
+            return new Vector3((float)x, (float)y, 1);
         }
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
