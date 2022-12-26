cbf9fd5cee8e2605552e38bc29f78c14cadcc6fb
Alex Pry <alhaffthemage@gmail.com>
2022-12-26 16:31:30 +0200

    Removed Step on axis u and v, becouse now they are stored in Cone class

--- CooridnateGrid/CooridnateGrid/Transformation/DecartToConeTransfromation.cs
+++ CooridnateGrid/CooridnateGrid/Transformation/DecartToConeTransfromation.cs
@@ -10,52 +10,12 @@ namespace CooridnateGrid.Transformation
 {
     public class DecartToConeTransfromation : Transformation
     {
-
-        private double _ellipseDenominator = 20;
-
-        private double _oneStepZAxis = 1.0 / 100;
         public Cone Cone { get; set; }
-        /// <summary>
-        /// Вісь u конуса, рухається вздовж еліпса від 0 до 2П
-        /// </summary>
-        public double OneStepOnEllipseAxis { get => Math.PI / EllipseDenominator; }
-        /// <summary>
-        /// Вісь v конуса, рухається прямою від -infinity до +infinity
-        /// </summary>
-        public double OneStepOnZAxis 
-        {
-            get => _oneStepZAxis; 
-            set
-            {
-                if (value != 0)
-                {
-                    _oneStepZAxis = value;
-                    OnPropertyChanged("OneStepOnZAxis");
-                }
-            }
-        }
-
-        
-        /// <summary>
-        /// Дільник на який ми ділимо П, для визначення одиничного кроку на осі u
-        /// </summary>
-        public double EllipseDenominator
-        {
-            get => _ellipseDenominator;
-            set 
-            {
-                if (_ellipseDenominator != 0)
-                {
-                    _ellipseDenominator = value;
-                    OnPropertyChanged("EllipseDenominator");
-                }
-            }
-        }
-
+     
         public override Func<Vector3, Vector3> Transform => p => 
         {
-            var u = p.X * OneStepOnEllipseAxis;
-            var v = p.Y * OneStepOnZAxis;
+            var u = p.X * Cone.OneStepOnEllipseAxis;
+            var v = p.Y * Cone.OneStepOnZAxis;
             return Cone.EllipsePoint(u, v);
         };
 
